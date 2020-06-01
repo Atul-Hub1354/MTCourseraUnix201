a1805784f1a04a91a52535da0ad246128a60c423
Atul-Hub1354 <65947192+Atul-Hub1354@users.noreply.github.com>
2020-06-01 21:08:41 +0530

    Updated the guessinggame.sh file
    Updated the guessinggame.sh file

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,44 +1,42 @@
-#!binsh
+#!/bin/sh
 
+#Function for guessing the number of files in the current directory
 function guessFiles {
-  
   #Reading user input
   while [ 1 ]
   do
-    echo Guess total number of files in current directory
+    echo "Guess total number of files in current directory"
     read number
-  
-    if [[ $number =~ ^[0-9]+$ ]]
+    
+    #Verify if input is an intger, if not then ask to re-enter
+    if ! [[ "$number" =~ ^[0-9]+$ ]]
     then
-       echo Only integer value and is accepted
-       continue
-    elif [ $number -ge 0 ]
-    then
-       echo Number should not be less than Zero
+       echo "Error: Only Integer is Accepted. Re-enter the number."
        continue
     else
        break
     fi
   done
   
-  totalFiles=$(ls -1  wc -l)
+  totalFiles=$(ls -1 | wc -l)    #<- File count in current directory
 }
  
+#Verifying if given inut is matching the file count or not
 while [ 1 ]
 do
 
     guessFiles  #- Executing the function
   
-    if [ $number -gt $totalFiles ]
+    if [[ $number -gt $totalFiles ]]
     then    
-        echo Your guess is too high
+        echo "Your guess is too high"
         continue
-    elif [ $number -lt $totalFiles ]
+    elif [[ $number -lt $totalFiles ]]
     then
-        echo Your guess is too low
+        echo "Your guess is too low"
         continue
     else
-        Congratulations!! You have gussed the correct number of files
-         exit 0
+        echo "Congratulations!! You have gussed the correct number of files"
+        exit 0
     fi
 done
